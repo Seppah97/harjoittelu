5286f39dc7ec1edb4adbc2f7674b6c6dffcd62b8
Seppä Henri <henri.seppa@insta.fi>
2023-04-04 13:47:57 +0300

    testing some new commands

--- test.sh
+++ test.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+
+
+printf "\nThis is a test, please wait.\n"
+
+start=`date +%s`
+
 echo Hello World
 
 curl http://example.com
+
+end=`date +%s`
+
+runtime=$((end-start))
+
+printf "\nAll completed!\n\nTime elapsed: %d\n" "${runtime}"
